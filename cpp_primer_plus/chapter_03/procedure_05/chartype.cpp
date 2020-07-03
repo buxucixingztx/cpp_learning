1980841eaf538f105999a0bbc7ddce91cdc8411e
buxucixingztx <695911088@qq.com>
2020-07-03 22:01:51 +0800

    添加了C++ Primer Plus中的第三章的例题及复习题

---
+++ cpp_primer_plus/chapter_03/procedure_05/chartype.cpp
@@ -0,0 +1,15 @@
+// chartype.cpp -- the char type
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    char ch;
+
+    cout << "Enter a character:\n";
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character.\n";
+
+    return 0;
+}
